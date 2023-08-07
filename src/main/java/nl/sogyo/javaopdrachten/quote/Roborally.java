002f416ab6ff8b213f6abd50ebfed2f6d8842d04
unknown <jvddonk@sogyo.nl>
2023-08-07 14:48:31 +0200

    Add complete version of roborally assignment

--- src/main/java/nl/sogyo/javaopdrachten/quote/Roborally.java
+++ src/main/java/nl/sogyo/javaopdrachten/quote/Roborally.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Robot {
     int positionX;
     int positionY;
     String facing;
 
-    public Robot(int positionX, int positionY, String facing) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.facing = facing;
+    public Robot(int x, int y, String f) {
+        positionX = x;
+        positionY = y;
+        facing = f;
     }
 
     public void printState() {
@@ -22,10 +25,10 @@ class Robot {
             facing = "East";
         } else if (facing == "East") {
             facing = "North";
-        } 
         }
-        
-        public void turnRight() {
+    }
+
+    public void turnRight() {
         if (facing == "North") {
             facing = "East";
         } else if (facing == "East") {
@@ -34,58 +37,57 @@ class Robot {
             facing = "West";
         } else if (facing == "West") {
             facing = "North";
-        } 
-        }
-        
-        public void forward() {
-        forward(1);
-        }
-        
-        public void forward(int speed) {
-            if (facing =="North") {
-                positionY += speed;
-            } else if (facing == "West") {
-                positionX -= speed;
-            } else if (facing == "South") {
-                positionY -= speed;
-            } else if (facing == "East") {
-                positionX += speed;
-            }
-            
-            
-         
-        }
-        
-           public void backward() {
-        backward(1);
-        }
-        
-        public void backward(int speed) {
-            if (facing =="North") {
-                positionY -= speed;
-            } else if (facing == "West") {
-                positionX += speed;
-            } else if (facing == "South") {
-                positionY += speed;
-            } else if (facing == "East") {
-                positionX -= speed;
-            }
-            
-            
-         
         }
     }
 
+    public void forward() {
+        forward(1);
+    }
+
+    public void forward(int speed) {
+        if (facing == "North") {
+            positionY += speed;
+        } else if (facing == "West") {
+            positionX -= speed;
+        } else if (facing == "South") {
+            positionY -= speed;
+        } else if (facing == "East") {
+            positionX += speed;
+        }
+    }
+
+    public void backward() {
+        backward(1);
+    }
+
+    public void backward(int speed) {
+        if (facing == "North") {
+            positionY -= speed;
+        } else if (facing == "West") {
+            positionX += speed;
+        } else if (facing == "South") {
+            positionY += speed;
+        } else if (facing == "East") {
+            positionX -= speed;
+        }
+    }
+}
 
 public class Roborally {
     public static void main(String[] args) {
         Robot myFirstRobot = new Robot(0, 1, "East");
         Robot mySecondRobot = new Robot(1, 0, "West");
 
-        myFirstRobot.printState();
-        myFirstRobot.turnLeft();
-        myFirstRobot.printState();
-        myFirstRobot.forward();
-        myFirstRobot.printState();
+        List<Runnable> listOfMethods = new ArrayList<>();
+
+        listOfMethods.add(myFirstRobot::printState);
+        listOfMethods.add(myFirstRobot::turnLeft);
+        listOfMethods.add(myFirstRobot::forward);
+        listOfMethods.add(() -> myFirstRobot.forward(2));
+        listOfMethods.add(myFirstRobot::printState);
+        
+        for (Runnable executeList : listOfMethods) {
+            executeList.run();
+        }
     }
 }

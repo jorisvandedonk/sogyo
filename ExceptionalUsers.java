4a4b045fdb58bf811381e3b34f4334e8843906e4
unknown <jvddonk@sogyo.nl>
2023-08-08 12:09:33 +0200

    Add Exceptional Users assignment

---
+++ ExceptionalUsers.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class ExceptionalUsers {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter a username\n");
+        String userName = userInput.nextLine();
+
+        System.out.print("Enter a password\n");
+        String passWord = userInput.nextLine();
+
+        while (!User.validPassword(passWord)) {
+            System.out.print("This is not a valid password. Please enter a stronger password.\n");
+            passWord = userInput.nextLine();
+        }
+        User user = new User(userName, passWord);
+        user.userRegistered();
+    }
+}
+
+final class User {
+    public final String userName;
+    public final String passWord;
+
+    public User(String u, String p) {
+        userName = u;
+        passWord = p;
+    }
+
+    public static boolean validPassword(String passWord) {
+        boolean hasNumber = false;
+        boolean hasLowerCase = false;
+        boolean hasUpperCase = false;
+
+        for (int i = 0; i < passWord.length(); i++) {
+            char character = passWord.charAt(i);
+            if (Character.isDigit(character)) {
+                hasNumber = true;
+            } else if (Character.isLowerCase(character)) {
+                hasLowerCase = true;
+            } else if (Character.isUpperCase(character)) {
+                hasUpperCase = true;
+            }
+        }
+
+        return hasNumber && hasLowerCase && hasUpperCase;
+    }
+    
+    public void userRegistered() {
+        System.out.println("Registered user " + userName);
+    }
+}
